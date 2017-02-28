eee2375bf18d4654575cbe9f7cbc74b49fc83f9e
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-28 15:49:00 -0700

    move query logic from controller to model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,7 @@
 class LinksController < ApplicationController
 
   def index
-    @links = Link.where('created_at > ?', 24.hours.ago).group(:url).count.sort_by{ |k, v| v }.reverse.first(10)
+    @links = Link.hottest_links
     respond_to do |format|
       format.html { @links = @links.to_h }
       format.json { render json: @links }
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,8 @@
 class Link < ApplicationRecord
   validates :url, presence: true
+
+  def self.hottest_links
+    where('created_at > ?', 24.hours.ago).group(:url).count.sort_by{ |k, v| v }.reverse.first(10)
+  end
+
 end
