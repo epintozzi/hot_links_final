89a79c81e5001f2b981d53b5500e1dde4aa6cf11
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-27 20:53:55 -0700

    send json to urllockbox

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,7 +1,11 @@
 class LinksController < ApplicationController
 
   def index
-    @links = Link.where('created_at > ?', 24.hours.ago).group(:url).count.sort_by{ |k, v| v }.reverse.first(10).to_h
+    @links = Link.where('created_at > ?', 24.hours.ago).group(:url).count.sort_by{ |k, v| v }.reverse.first(10)
+    respond_to do |format|
+      format.html { @links = @links.to_h }
+      format.json { render json: @links }
+    end
   end
 
 end
